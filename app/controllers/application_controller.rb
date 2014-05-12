9d8e6791e6675bbd84bb27dc0556961603aad764
neil patel <neil@myindianweddingapp.com>
2014-05-12 19:53:46 +0100

    Set user permissions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation) }
    end
 end
